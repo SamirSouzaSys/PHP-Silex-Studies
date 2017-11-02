0b73f0550413f949f283edd0560519fdd9ea1a5d
samirsouzasys <samir.guitar@gmail.com>
2017-11-02 15:01:21 -0300

    #3 Middlewares

--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 require __DIR__ . "/../vendor/autoload.php";
 
 use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 $app = new Application();
 
@@ -9,13 +11,43 @@ $app->get('/', function () {
     return "Helllo world com Silex";
 });
 
-$app->get('/categories/{slug}', function ($slug) {
-    return "Categoria: ".$slug ;
-})
-    ->value('slug','Valor default')
-    ->convert('slug',function($slug){
-    return (int)$slug;
+/*
+- Aplicação
+Before, After, Finish
+*/
+/*$app->before(function (Request $request) {
+    print "Middleware Before - ";
 });
+
+$app->after(function (Request $request, Response $response) {
+    print "Middleware After - ";
+});
+
+$app->finish(function () {
+    print "Middleware Finish - LogSaved";
+});*/
+
+$app->get('/categories/{slug}', function ($slug) {
+    return "Categoria: " . $slug;
+})
+    ->value('slug', 'Valor default')
+    ->convert('slug', function ($slug) {
+        return (int)$slug;
+    })
+
+    /* Aula 4 - Middlewares
+    - Rotas
+    Before, After
+    */
+    ->before(function (Request $request) {
+        print "Middleware Main Route Categories 1 - ";
+    })
+    ->before(function (Request $request) {
+        print "Middleware Main Route Categories 2 - ";
+    })
+    ->after(function (Request $request, Response $response) {
+        print "Middleware Main Route After - ";
+    });
 
 
 $app->run();
